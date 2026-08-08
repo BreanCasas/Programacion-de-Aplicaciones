e5cd27ec2c72ef39f0a9bc36d943d3c3148a9030
BreanCasas <brean.casas@estudiantes.utec.edu.uy>
2026-08-08 15:38:56 -0300

    Create dasdasd.java

---
+++ dasdasd.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author brean
+ */
+public class dasdasd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("hoola");
+System.out.println("hoola");
+System.out.println("hoola");
+//cofjdsiofmdsplfmf
+        System.out.println("hoola");
+System.out.println("hoola");
+
+    }
+}
