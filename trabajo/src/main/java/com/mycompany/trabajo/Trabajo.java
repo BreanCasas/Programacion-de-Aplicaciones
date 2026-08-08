8953121c0838d04b20c40e2e75465a0d8a2f1865
BreanCasas <brean.casas@estudiantes.utec.edu.uy>
2026-08-08 16:07:32 -0300

    jframes
    en teoria creo que agrege el tema de los jframe pero falta fijarse si en otra compu anda bien

---
+++ trabajo/src/main/java/com/mycompany/trabajo/Trabajo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo;
+
+/**
+ *
+ * @author brean
+ */
+public class Trabajo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //probando probando
+    }
+}
